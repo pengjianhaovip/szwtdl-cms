cf3df9a199cbdeeb6d8f9ce2d11503aae8fb066c
pengjianhaovip <pengjianhaovip@163.com>
2018-12-03 17:16:21 +0800

    fix: 初始化项目

---
+++ app/Http/Controllers/NewsCateController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\NewsCate;
+use Illuminate\Http\Request;
+
+class NewsCateController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\NewsCate  $newsCate
+     * @return \Illuminate\Http\Response
+     */
+    public function show(NewsCate $newsCate)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\NewsCate  $newsCate
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(NewsCate $newsCate)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\NewsCate  $newsCate
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, NewsCate $newsCate)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\NewsCate  $newsCate
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(NewsCate $newsCate)
+    {
+        //
+    }
+}
---
+++ database/migrations/2018_12_03_091457_create_product_cates_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductCatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_cates', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_cates');
+    }
+}
